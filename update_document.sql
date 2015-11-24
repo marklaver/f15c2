a26856a67d64f619858104aca71a41b63fb26509
Mark Laver <marklaver@users.noreply.github.com>
2015-11-23 21:35:32 -0600

    Update update_document.sql

--- update_document.sql
+++ update_document.sql
@@ -1,4 +1,4 @@
-create or replace trigger update_document after insert on F15C2_document
+create or replace trigger update_document before insert on F15C2_document
 for each row
 declare
     pos          number;
@@ -7,18 +7,17 @@ declare
     mime_type    VARCHAR2(512);
     v_charset    VARCHAR2(512);
 begin
-    if updating then
+    v_charset := '';
     select UTL_RAW.CAST_TO_VARCHAR2(:new.file_blob) into whole_name from F15C2_document;
     select instrb(whole_name, '.', 1, 1) into pos from dual;
     select substr(whole_name, 1, pos-1) into file_name from dual;
     select substr(whole_name, pos+1) into mime_type from dual;
-        if mime_type = 'txt' then
+    if mime_type = 'txt' then
             v_charset := 'ASCII';
-            update F15C2_document set file_charset = 'ASCII' where doc_id = :new.doc_id;
-            end if;
-    update F15C2_document set filename = file_name,
-                              file_mimetype = mime_type
-                              where doc_id = :new.doc_id;
     end if;
+    :new.f15c2_rfe_rfe_id := v('P2_RFE');
+    :new.filename := file_name;
+    :new.file_mimetype := mime_type;
+    :new.file_charset := v_charset;
 end;
 /
